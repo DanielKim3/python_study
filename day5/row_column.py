a75fae7d60d7869cc98740310bac97669c7da62f
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-03-27 15:56:11 +0900

    Update row_column.py

--- day5/row_column.py
+++ day5/row_column.py
@@ -2,12 +2,16 @@ s = ""
 l = 0
 try:
   n = float (input("행을 입력하세요 : "))
-  m = float (input("열을 입력하세요: "))
-  for i in range(int(n)):
-    for j in range(1, int(m) + 1):
-      s += str(j + l) + " "
-    s += '\n'
-    l += 1
-  print(s)
+  m = float (input("열을 입력하세요 : "))
+  if (n % 1 != 0) | (m % 1 != 0 ) | (n <= 0) | ( m <= 0):
+    print("에러 발생")
+  else:
+    for i in range(int(n)):
+      for j in range(1, int(m) + 1):
+        s += str(j + l) + " "
+      s += '\n'
+      l += 1
+    print(s)
 except ValueError:
   print("에러 발생")
+
