ef2e9af984cef8e818a4fefcc5c4dbc06345ca03
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-03-27 14:18:12 +0900

    Update stars.py

--- day4/stars.py
+++ day4/stars.py
@@ -1,4 +1,11 @@
 s = "*"
-n = float(input())
-for i in range(1, n + 1):
-  print(i * s)
+try:
+  n = int(input())
+  if n < 0:
+    print("음수는 사용할 수 없습니다")
+  else:
+    for i in range(1,n + 1):
+     print(i * s)
+except ValueError:
+  print("에러 발생")
+  
