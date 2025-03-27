c853e66e7b44f143f6f5787c711562e898b98352
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-03-27 13:56:54 +0900

    Update number_pyramid.py

--- day5/number_pyramid.py
+++ day5/number_pyramid.py
@@ -1,9 +1,9 @@
 s = ""
 p = 1
 n = int(input('숫자를 입력해주세요 : '))
-if(n <= 9 and n >= 0):
+if(n <= 9 and n > 0):
   for i in range(1):
-      for j in range(1, n):
+      for j in range(1, n+1):
         if j == 1:
           s += ' ' * (2)
           s += str(p)
@@ -24,5 +24,5 @@ if(n <= 9 and n >= 0):
           p += 1
   print(s)
 else:
-  print("10 미만으로 작성하시오.")
+  print("1이상 10 미만으로 작성하시오.")
 
